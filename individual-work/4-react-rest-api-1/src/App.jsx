87740168603d36f37660d8c20fcaa68001224f1f
MasterJay8 <jirka.kocour100@gmail.com>
2023-02-05 18:51:27 +0100

    TODO

--- individual-work/4-react-rest-api-1/src/App.jsx
+++ individual-work/4-react-rest-api-1/src/App.jsx
@@ -1,9 +1,21 @@
 import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 
+const TodoItem = ({ todo }) => {
+  return (
+    <div className="bg-gray-800 text-white rounded-lg shadow-lg text-center m-[5px]">
+      <h3>{todo.title}</h3>
+      <p>User ID: {todo.userId}</p>
+      <p>ID: {todo.id}</p>
+    </div>
+  );
+};
+
 function App() {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+
+  document.body.style = 'background: black;';
 
   const fetchData = async () => {
     setLoading(true);
@@ -26,17 +38,13 @@ function App() {
       <h1>React App</h1>
       {loading ? (
         <p>Loading...</p>
-      ) : (
-        <div>
-          {data ? (
-            <div>
-              {data.map((item) => (
-                <div key={item.id}>
-                  <p>{item.title}</p>
-                </div>
-              ))}
-            </div>
-          ) : (
+        ) : (
+          <div className="grid gap-2 grid-cols-3">
+            {data ? (
+              data.map((todo) => (
+                <TodoItem key={todo.id} todo={todo} />
+              ))
+            ) : (
             <p>No data</p>
           )}
         </div>
